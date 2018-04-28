f2c688a44794c6ca18420091441ef4cb65ce4507
Anthony Ruiz <yobakaj@gmail.com>
2018-04-28 15:23:50 -0400

    Initial Commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Anthony Ruiz on 4/24/18.
+//  Copyright © 2018 Jakaboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        print(sender.tag)
+        
+    }
+    
+  
+
+}
+
